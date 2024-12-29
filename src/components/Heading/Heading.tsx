74b07df210a8808e628afdd870b1e5adf2093d7e
nk-on <2nkoz@proton.me>
2024-12-28 22:16:23 -0800

    Style:creating and styling heading section

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,10 @@
+export default function Heading(){
+    return (
+        <div className="w-full h-[30%] bg-[#343A3F] text-[#fff] flex justify-around	 items-center">
+             <h1>Rock paper scissors</h1>
+             <div className="hover:bg-[#000] w-[90px] rounded-[5px] flex justify-center items-center">
+               <button>Instuctions</button>
+             </div>
+        </div>
+    )
+}
