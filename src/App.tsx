b42c90744a11f970382f00d925e1594b5b361235
nk-on <2nkoz@proton.me>
2024-12-30 08:09:23 -0800

    Feat:adding startMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Heading from './components/Heading/Heading'
+import StartMenu from './components/startMenu/startMenu'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Heading />
+      <StartMenu />
     </>
   )
 }
---
+++ src/components/startMenu/startMenu.tsx
@@ -0,0 +1,8 @@
+export default function StartMenu(){
+    return (
+        <div className="text-center flex flex-col  items-center gap-[10px] w-[100%] mt-[30px]">
+            <h1 className="text-[30px]">Welcome to rock paper scissors game</h1>
+            <button className="bg-[#000] text-[#fff] p-[20px] rounded-[5px]">Click here</button>
+        </div>
+    )
+}
