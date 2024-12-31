83873ce4b855cb112b3d6b757e6330f3f15cf717
nk-on <johndoe@example.com>
2024-12-31 22:37:25 +0400

    Feat:determinign winner between user and computer

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,32 +1,48 @@
-import optionsArray from "./optionsArray"
-import Option from "./Option"
-import { useState } from "react"
-export default function Game(){
-    {/* 1. user should be able to choose which option they want 
+import optionsArray from './optionsArray';
+import Option from './Option';
+import { useState } from 'react';
+export default function Game() {
+  {
+    /* 1. user should be able to choose which option they want 
         2. computer should be able to generete side 
         3. Rock wins against scissors; paper wins against rock; and scissors wins against paper
         4. in each turn computer should declare winner and add point to the side
         
-         */}
-    const [playerChoice,setPlayerChoice] = useState("");
-    const [computerChoice,setComputerChoice] = useState("");
-    return (
-        <div className="w-[100%] h-[350px] grid grid-cols-1">
-            <div className="flex justify-evenly text-[30px]">
-                <div>
-                    <h1>Player</h1>
-                    <div>{playerChoice}</div>
-                </div>
-                <div>
-                    <h1>Computer</h1>
-                    <div>{computerChoice}</div>
-                </div>
-            </div>
-            <div className="flex justify-evenly text-[30px] cursor-pointer">
-                {optionsArray.map((option)=>{
-                    return <Option optionEmoji = {option.emoji} setPlayerChoice = {setPlayerChoice} setComputerChoice = {setComputerChoice} />
-                })}
-            </div>
+         */
+  }
+  const [playerChoice, setPlayerChoice] = useState('');
+  const [computerChoice, setComputerChoice] = useState('');
+  return (
+    <div className="w-[100%] h-[350px] grid grid-cols-1">
+      <div className="flex justify-evenly text-[30px]">
+        <div>
+          <h1>Player</h1>
+          <div>{playerChoice}</div>
         </div>
-    )
+        <div>{optionsArray.map((option) => {
+            if(option.emoji === playerChoice && option.beats === computerChoice){
+                return "Player"
+            };
+            if(option.emoji === computerChoice && option.beats === playerChoice){
+                return "Computer"
+            };
+        })}</div>
+        <div>
+          <h1>Computer</h1>
+          <div>{computerChoice}</div>
+        </div>
+      </div>
+      <div className="flex justify-evenly text-[30px] cursor-pointer">
+        {optionsArray.map((option) => {
+          return (
+            <Option
+              optionEmoji={option.emoji}
+              setPlayerChoice={setPlayerChoice}
+              setComputerChoice={setComputerChoice}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
