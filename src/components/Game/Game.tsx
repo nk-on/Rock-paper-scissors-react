169b9b0d077e5b2b94a7c9b3a539e646a31065a7
nk-on <johndoe@example.com>
2025-01-01 20:19:22 +0400

    Adding points tracking system

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,6 +1,6 @@
 import optionsArray from './optionsArray';
 import Option from './Option';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 export default function Game() {
   {
     /* 1. user should be able to choose which option they want 
@@ -12,16 +12,29 @@ export default function Game() {
   }
   const [playerChoice, setPlayerChoice] = useState('');
   const [computerChoice, setComputerChoice] = useState('');
+  const [playersPoint, setPlayersPoint] = useState(0);
+  const [computersPoint, setComputersPoint] = useState(0);
   const draw = useRef(false);
+  useEffect(() => {
+    optionsArray.forEach((option) => {
+      if (option.emoji === playerChoice && option.beats === computerChoice) {
+        setPlayersPoint(playersPoint + 1);
+      }
+      if (option.emoji === computerChoice && option.beats === playerChoice) {
+        setComputersPoint(computersPoint + 1);
+      }
+    });
+  }, [playerChoice, computerChoice]);
   return (
     <div className="w-[100%] h-[350px] grid grid-cols-1">
       <div className="flex justify-evenly text-[30px]">
         <div>
+          <span>{playersPoint}</span>
           <h1>Player</h1>
           <div>{playerChoice}</div>
         </div>
         <div>
-          {draw.current ? "Draw" : ""}
+          {draw.current ? 'Draw' : ''}
           {optionsArray.map((option) => {
             if (
               option.emoji === playerChoice &&
@@ -40,6 +53,7 @@ export default function Game() {
           })}
         </div>
         <div>
+          <span>{computersPoint}</span>
           <h1>Computer</h1>
           <div>{computerChoice}</div>
         </div>
@@ -48,7 +62,7 @@ export default function Game() {
         {optionsArray.map((option) => {
           return (
             <Option
-              draw = {draw}
+              draw={draw}
               optionEmoji={option.emoji}
               setPlayerChoice={setPlayerChoice}
               setComputerChoice={setComputerChoice}
