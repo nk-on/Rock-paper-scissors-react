879d8ef992383adb08443ec36c3f7a76e925f118
nk-on <2nkoz@proton.me>
2025-01-16 22:01:07 -0800

    Deleting unnecessary variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
 import Heading from './components/Heading/Heading'
 import StartMenu from './components/startMenu/startMenu'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
